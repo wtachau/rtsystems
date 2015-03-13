5243d9c1f373a06566e5696f6b0c852cea07fb08
Will Tachau <wtachau@gmail.com>
2015-03-12 22:39:31 -0700

    initial commit

---
+++ home_page/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from home_page_app.views import *
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'home_page.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', home)
+)
